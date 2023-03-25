3a51ee93459149d970d88dff44e0b57c2971ef97
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2023-03-24 17:41:13 -0700

    squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,17 +2,18 @@
 
 /**
  * print_square - prints a square of size size
- * @size: size of the sqaure
+ * @size: size of the square
  *
  * Return: void
  */
 
-void print_sqaure(int size)
+void print_square(int size)
 {
 	int i, j;
 
 	if (size <= 0)
 		_putchar('\n');
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size); j++)
