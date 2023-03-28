f948a59880fb644fec7e0592bddb344730c17fdd
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2023-03-28 02:38:51 -0700

    put

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' ; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (i % 10 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
