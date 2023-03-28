87404b93146516b00c27a36765047af4851f4717
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2023-03-28 03:11:01 -0700

    print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count ; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = 0 ; i < count; i++)
+		_putchar(s[count - i - 1]);
 	_putchar('\n');
 }
