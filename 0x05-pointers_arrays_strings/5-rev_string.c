afdb0f48894efba1e2a84a2d2447c5e39a43b3fa
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2023-03-29 17:47:31 -0700

    print

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,12 @@
 
 void rev_string(char *s)
 {
-	char rev = s[0];
-	char counter = 0;
 	int i;
+	int count = 0;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
-	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
-	}
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = 0 ; i < count; i++)
+		_putchar(s[count - i - 1]);
+	_putchar('\n');
 }
